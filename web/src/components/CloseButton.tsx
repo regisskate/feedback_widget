9132f3210f5190fd5fbc3576e381bab381ba00cc
Reginaldo J. Santos <freestreetskate@gmail.com>
2022-05-07 05:02:54 -0300

    botão de feedback

---
+++ web/src/components/CloseButton.tsx
@@ -0,0 +1,10 @@
+import { Popover } from "@headlessui/react";
+import { X } from "phosphor-react";
+
+export function CloseButton() {
+  return (
+    <Popover.Button className="Top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fechar formulario">
+      <X weight="bold" className="w-4 h-4" />
+    </Popover.Button>
+  )
+}
